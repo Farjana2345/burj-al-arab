3db9a74444e7fd3c7c1f59ea3d3983d503ce383e
Farjana2345 <fa8547830@gmail.com>
2021-03-26 13:51:25 +0100

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmea0LegAThIHK8qk1ntbreWMu4YLEtw8",
+    authDomain: "burj-al-arab-9e5a4.firebaseapp.com",
+    projectId: "burj-al-arab-9e5a4",
+    storageBucket: "burj-al-arab-9e5a4.appspot.com",
+    messagingSenderId: "894324450908",
+    appId: "1:894324450908:web:f9afd5076257a01a496899"
+  };
+  export default firebaseConfig;
